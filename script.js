9e1be322ce7ae5ab31a3ad058e76888d55c9ea9c
PatricioCisneros <al45760@un.edu.mx>
2025-11-15 18:15:39 -0600

    avance en el script

---
+++ script.js
@@ -0,0 +1,24 @@
+function mostrarCotizacion(){
+
+    var nombre = document.getElementById("nombre").value;
+    var correo = document.getElementById("correo").value;
+    var marca = document.getElementById("marca").value;
+
+    //FINANCIAMIENTO
+    var tipoFinanciamiento = document.querySelector("input[name='financiamiento']:checked").value
+
+    //EXTRAS
+    var extrasSelecionados = [];
+    document.querySelectorAll("input[type='checkbox']: checked").forEach(c => {
+        extrasSelecionados.push(c.value);
+    });
+
+    //MOSTRAR EL RESULTADO
+    document.getElementById("resultado").innerHTML = 
+    "<h3>Cotizacion generada:</h3>" + 
+    "<p><strong>Nombre:</strong> " + nombre + "</p>" + 
+    "<p><strong>Correo:</strong> " + correo + "</p>" +  
+    "<p><strong>Financiamiento:</strong> " + tipoFinanciamiento + "</p>" + 
+    "<p><strong>Marca seleccionada:</strong> " + marca + "</p>" +
+    "<p><strong>Extras:</strong> " + (extrasSelecionados.join(", ") || "Ninguno") + "</p>"; 
+}
